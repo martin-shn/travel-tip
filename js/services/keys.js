a32961d24030c514a61757c1e600f3c1fc3f4b2d
Martin Sh <martinshn@gmail.com>
2021-08-18 22:38:31 +0300

    added keys

---
+++ js/services/keys.js
@@ -0,0 +1,3 @@
+const GEO_LOCATION_API = 'AIzaSyDgw0mWmcS4OoFUyLUj5oNbfo4KGzpHiYA';
+const MAPS_API = 'AIzaSyCjlsAsJI0Vd-wbFqIbs_zppWj9Txa2X2E';
+const WEATHER_KEY = '5cd4235d1578c79bbfc3defa60d714b7';
